d936125f4b6f5173bdd94ec4359551e74d618cbe
Jorge humberto espinosa Salvatella <134461073+jorgesalvatella@users.noreply.github.com>
2025-02-14 00:26:43 -0500

    arreglo en viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/anteproyecto-rn/',  // Asegura que los archivos se sirvan desde el subdirectorio adecuado
 });
